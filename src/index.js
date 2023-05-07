e8eef08ab1b616a7e64088d4aaa79db95f7a88d8
viniciusMartinsDev <mpviniciusmp@gmail.com>
2023-05-07 11:49:01 -0300

    fix: Organizando pastas

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ const express = require('express')
 
 const app = express()
 
-app.use('/', (req, res) => {
-	res.send('Hello World!')
-})
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+const routeTeste = require('./routes/teste')
+
+app.use(routeTeste)
 
 app.listen(3000)
+console.log('Servidor iniciado')
